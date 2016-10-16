e7ec4eb793b7ac3a0a6bc021489623b558401804
Alexey Kuntsevich <alexey.kuntsevich@gmail.com>
2016-10-16 12:42:01 +0200

    Tailrecced ex2.1, implemented ex2.2

--- src/test/scala/exercises.scala
+++ src/test/scala/exercises.scala
@@ -3,20 +3,47 @@ import org.scalatest._
 
 class BookCh2 extends FlatSpec with Matchers {
 
-  "Ex2.1" should "implement a recursive fibonaci function" in {
+  "Ex2.1" should "implement a recursive fibonacci function with tail recursion" in {
     
     def fib(n: Int): Int = {
-      n match {
-        case 0 => 0
-        case 1 => 1
-        case _ => fib(n-2) + fib(n-1)
+
+      @annotation.tailrec
+      def fib_int(idx: Int, a: Int, b: Int): Int = {
+        idx match {
+          case 0 => a
+          case _ => fib_int(idx - 1, b, a + b)
+        }
       }
+
+      fib_int(n, 0, 1)
     }
 
     fib(0) should be (0)
     fib(1) should be (1)
     fib(3) should be (2)
     fib(5) should be (5)
+  }
+
+  "Ex2.2" should "implement an isSorted function that checks if array is sorted according to comparison function" in {
+    def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+      @annotation.tailrec
+      def isSortedFrom(n: Int): Boolean = {
+        if (n>(as.length - 2)) true
+        else if (!ordered(as(n), as(n+1))) false
+        else isSortedFrom(n+1)
+      }
+
+      isSortedFrom(0)
+
+    }
+
+    isSorted(Array(1,2,3), (a:Int,b:Int) => {a < b}) should be (true)
+    isSorted(Array(1,2), (a:Int,b:Int) => {a < b}) should be (true)
+    isSorted(Array(2,1), (a:Int,b:Int) => {a < b}) should be (false)
+    isSorted(Array(2,1,3), (a:Int,b:Int) => {a < b}) should be (false)
+    isSorted(Array(1,3,2), (a:Int,b:Int) => {a < b}) should be (false)
+
+
   }
   
 }
