e18807bb721b9df97bd160488c258366724d16cc
Alexey Kuntsevich <alexey.kuntsevich@gmail.com>
2016-10-16 13:46:06 +0200

    Implement a tail function

--- src/test/scala/BookCh3.scala
+++ src/test/scala/BookCh3.scala
@@ -2,29 +2,38 @@ import org.scalatest._
 
 class BookCh3 extends FlatSpec with Matchers {
 
+  sealed trait List[+A]
+  case object Nil extends List[Nothing]
+  case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+  object List {
+    def sum(ints: List[Int]): Int = ints match {
+      case Nil => 0
+      case Cons(x, xs) => x + sum(xs)
+    }
+
+    def product(ds: List[Double]): Double = ds match {
+      case Nil => 1.0
+      case Cons(0.0, _) => 0.0
+      case Cons(d, dbs) => d * product(dbs)
+    }
+
+    def apply[A](as: A*): List[A] =
+      if (as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+
+    def tail[A](l: List[A]): List[A] = {
+      l match {
+        case Nil => Nil
+        case Cons(h, t) => t
+      }
+    }
+  }
+
+
 
   "Ex3.1" should "implement a recursive fibonacci function with tail recursion" in {
 
-    sealed trait List[+A]
-    case object Nil extends List[Nothing]
-    case class Cons[+A](head: A, tail: List[A]) extends List[A]
-
-    object List {
-      def sum(ints: List[Int]): Int = ints match {
-        case Nil => 0
-        case Cons(x, xs) => x + sum(xs)
-      }
-
-      def product(ds: List[Double]): Double = ds match {
-        case Nil => 1.0
-        case Cons(0.0, _) => 0.0
-        case Cons(d, dbs) => d * product(dbs)
-      }
-
-      def apply[A](as: A*): List[A] =
-        if (as.isEmpty) Nil
-        else Cons(as.head, apply(as.tail: _*))
-    }
 
     val x = List(1,2,3,4,5) match {
       case Cons(x, Cons(2, Cons(4, _))) => x
@@ -35,6 +44,14 @@ class BookCh3 extends FlatSpec with Matchers {
     }
 
     x should be (3)
+  }
+
+  "Ex2.3" should "implement a tail function" in {
+
+    List.tail(List(1,2,3)) should be (List(2,3))
+    List.tail(List(1)) should be (Nil)
+    List.tail(Nil) should be (Nil)
+
   }
   
 }
