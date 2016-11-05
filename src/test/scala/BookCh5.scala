7b32a68ecd491d10d9832fe195881580f403085e
Alexey Kuntsevich <alexey.kuntsevich@gmail.com>
2016-11-05 20:28:03 +0100

    unfolded till ex5.12

--- src/test/scala/BookCh5.scala
+++ src/test/scala/BookCh5.scala
@@ -84,9 +84,32 @@ class BookCh5 extends FlatSpec with Matchers {
 		def apply[A](as: A*): Stream[A] =
 			if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 
+		def constant[A](a: A): Stream[A] =
+			Stream.cons(a, constant(a))
+
+		def from(n: Int): Stream[Int] = Stream.cons(n, from(n+1))
+		def fibs: Stream[Int] = {
+			def fibsInt(a: Int, b: Int): Stream[Int] = Stream.cons(a, fibsInt(b, a + b))
+
+			fibsInt(0, 1)
+		}
+
+		def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = 
+			f(z) match {
+				case Some((a,s)) => Stream.cons(a, unfold(s)(f))
+				case None => Stream.empty
+			}
+
+		def ones: Stream[Int] = Stream.cons(1, ones)
+
+		def onesUF: Stream[Int] = Stream.unfold(1)(s => Some(1,1))
+		def constantUF[A](a: A): Stream[A] = Stream.unfold(a)(s => Some(a,a))
+		def fibsUF: Stream[Int] = Stream.unfold((0,1))(s => Some((s._1, (s._2, s._1 + s._2))))
+		def fromUF(n: Int): Stream[Int] = Stream.unfold(n)(s => Some(s, s + 1))
+
 	}
 
-	val ones: Stream[Int] = Stream.cons(1, ones)
+	
 
 	"Ex5.1" should "implement toList function that produces list from a stream" in {
 		Stream(1,2,3,4,5).toList should be (List(1,2,3,4,5))
@@ -111,7 +134,7 @@ class BookCh5 extends FlatSpec with Matchers {
 		Stream(1,2,7,3).forAll(_<5) should be (false)
 		Stream(1,2,3).forAll(_<5) should be (true)
 
-		ones.forAll(_ == 0) should be (false)
+		Stream.ones.forAll(_ == 0) should be (false)
 	}
 
 	"Ex5.5" should "implement takeWhile using foldRight" in {
@@ -132,4 +155,36 @@ class BookCh5 extends FlatSpec with Matchers {
 		Stream(1,2).flatMap(e => Stream(e, e+1)).toList should be (List(1,2,2,3))
 
 	}
+
+	"Ex5.8" should "implement function constant producing an infinite stream of input parameter instances" in {
+		Stream.constant(2).take(3).toList should be (List(2,2,2))
+		Stream.constant(2).take(5).toList should be (List(2,2,2,2,2))
+	}
+
+	"Ex5.9" should "implement a function producing an infinite stream of monotonically increasing integers starting from n" in {
+		Stream.from(3).take(3).toList should be (List(3,4,5))
+		Stream.from(2).take(5).toList should be (List(2,3,4,5,6))
+	}
+
+	"Ex5.10" should "implement fibonacci stream generation" in {
+		Stream.fibs.take(5).toList should be (List(0,1,1,2,3))
+		Stream.fibs.take(7).toList should be (List(0,1,1,2,3,5,8))
+	}
+
+	"Ex5.11" should "implement unfold" in {
+		Stream.unfold(0)(a => if (a < 10) Option((a + 2, a + 2)) else None).toList should be (List(2,4,6,8,10))
+	}
+
+	"Ex5.12" should "implement fibs, from, constant and ones using unfold" in {
+		Stream.constantUF(2).take(3).toList should be (List(2,2,2))
+		Stream.constantUF(2).take(5).toList should be (List(2,2,2,2,2))
+		Stream.fromUF(3).take(3).toList should be (List(3,4,5))
+		Stream.fromUF(2).take(5).toList should be (List(2,3,4,5,6))
+		Stream.fibsUF.take(5).toList should be (List(0,1,1,2,3))
+		Stream.fibsUF.take(7).toList should be (List(0,1,1,2,3,5,8))
+		Stream.onesUF.take(5).toList should be (List(1,1,1,1,1))
+		Stream.onesUF.take(7).toList should be (List(1,1,1,1,1,1,1))
+	}
+
+
 }
