0c116ec9f3e00a12cb8565f1e9cebd24509a9149
Alexey Kuntsevich <alexey.kuntsevich@gmail.com>
2017-01-22 19:24:11 +0100

    Add some API draft according to Ex7.2

--- src/test/scala/BookCh7.scala
+++ src/test/scala/BookCh7.scala
@@ -4,6 +4,15 @@ class BookCh7 extends FlatSpec with Matchers {
 
 	class Par[A] {}
 
+	object Par {
+		def unit[A](a: A): Par[A] = ???
+		def map2[A,B,C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
+		def fork[A](a: => Par[A]): Par[A] = ???
+		def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+		def run[A](a: Par[A]): A = ???
+
+	}
+
 	"Ex7.1" should "define a signature for map2" in {
 		def map2[A,B,C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
 	}
